8496d8c92db455f852d3d137b443bbb3b8014004
Neil Mehta <neil.mehta310501@gmail.com>
2025-04-03 17:39:51 +0530

    add the login and signup dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { TaskModule } from './task/task.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { CommonModule } from './common/common.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      uri: configService.get<string>('MONGODB_URI') || 'mongodb://localhost:27017/task-manager'
+    }),
+    inject: [ConfigService]
+  }), AuthModule, TaskModule, ThrottlerModule.forRoot({
+    throttlers: [{
+      ttl: 60000,
+      limit: 10
+    }]
+  }), CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SignupDto {
+  @ApiProperty({example: 'John Doe'})
+  name: string;
+
+  @ApiProperty({ example: 'john@example.com' })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({ example: 'password123' })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
+
