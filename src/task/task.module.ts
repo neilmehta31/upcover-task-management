a292214679870a599d430541e6d2190afed69443
Neil Mehta <neil.mehta310501@gmail.com>
2025-04-03 19:51:15 +0530

    add import for mongose and authguard

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Task, TaskSchema } from 'src/schema/tasks.schema';
+import { User, UserSchema } from 'src/schema/user.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Task.name, schema: TaskSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+    AuthModule,
+  ],
   providers: [TaskService],
   controllers: [TaskController]
 })
